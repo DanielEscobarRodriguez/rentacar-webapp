f5fb1f327ca482815d6a60682f1bd94ae0b8a929
DanielEscobarRodriguez <descorod@myuax.com>
2024-07-04 17:59:28 +0200

    ultimos retoques

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import Layout from '../layout'; // Asegúrate de que la ruta del import es correcta
+import styles from './page.module.css';
 
 export default function Home() {
     return (
-        <Layout>
-            <header>
+        <div>
+            <header className={styles.header}>
                 <h1>Bienvenido a Rent-A-Car</h1>
             </header>
-            <section>
+            <section className={styles.section}>
                 <p>¡Explora, gestiona y más!</p>
                 <p>Navega a través del menú para comenzar a gestionar los vehículos, clientes y alquileres.</p>
             </section>
-            <footer>
+            <footer className={styles.footer}>
                 <div>© 2024 Rent-A-Car. Todos los derechos reservados.</div>
                 <div>Contacto: info@rentacar.com</div>
             </footer>
-        </Layout>
+        </div>
     );
 }
-
